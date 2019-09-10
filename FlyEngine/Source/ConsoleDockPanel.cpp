4bee9b255edae9afc512a94db63a09200a7fc090
Roger Tello Araez <rogerteello97@gmail.com>
2019-09-10 17:43:09 +0200

    console wip

--- FlyEngine/Source/ConsoleDockPanel.cpp
+++ FlyEngine/Source/ConsoleDockPanel.cpp
@@ -5,11 +5,13 @@
 ConsoleDockPanel::ConsoleDockPanel(bool isVisible) : DockPanel("Console", isVisible)
 {
 	flyEngineSection = FLY_SECTION_BOTH;
-	dockPanelType = DOCK_CONSOLE; 
+	dockPanelType = DOCK_CONSOLE;
+
 }
 
 ConsoleDockPanel::~ConsoleDockPanel()
 {
+
 }
 
 bool ConsoleDockPanel::Draw()
@@ -20,44 +22,55 @@ bool ConsoleDockPanel::Draw()
 		return false;
 #pragma endregion
 
-	if (ImGui::Begin(panelName.c_str(), &visible)) {
+	if (ImGui::Begin(panelName.c_str(), &visible, ImGuiWindowFlags_NoScrollbar)) {
 		
+		// Debug Button
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.75f, 0.75f, 0.75f, 1));
 		if (ImGui::Button("Debug")) {
 
 		} 
+		ImGui::PopStyleColor();
 
-		ImGui::SameLine(); 
+		// Warnings Button
+		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.8f, 0.4f, 1)); 
 		if (ImGui::Button("Warnings")) {
 
 		}
+		ImGui::PopStyleColor(); 
 
+		// Errors Button
 		ImGui::SameLine();
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 0.2f, 0.2f, 1));
 		if (ImGui::Button("Errors")) {
 
 		}
+		ImGui::PopStyleColor(); 
 
+		// Clear Button
 		ImGui::SameLine();
+		ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 5.0f);
+		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.2f, 0.2f, 1));
 		if (ImGui::Button("Clear")) {
 
 		}
 
-		ImGui::Separator();
+		ImGui::PopStyleColor(); 
+		ImGui::PopStyleVar(); 
 
-		ImGui::BeginChild("ConsoleMessagesArea", ImVec2(ImGui::GetWindowWidth() - 100, ImGui::GetWindowHeight()), true); 
+		// Messages Area
+		ImGui::Separator();
+		ImGui::BeginChild("ConsoleMessagesArea", ImVec2(ImGui::GetWindowContentRegionWidth(), ImGui::GetWindowHeight() - ImGui::GetCursorPosY() * 1.6));
 
 		ImGui::Text("Messages Should Lay Here");
 
 		ImGui::EndChild();
-
 		ImGui::Separator();
 
+		// Bottom Bar
 		if (ImGui::Button("Search")) {
 
 		}
-
-		//ImGui::SameLine(); 
-
-		//ImGui::InputText("SearchFilterBox", );
 	
 	}
 
