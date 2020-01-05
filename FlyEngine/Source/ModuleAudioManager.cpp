cc7a88e52fc3502c29564494bd7b2f963c296e49
rogerta <rogerteello97@gmail.com>
2020-01-05 20:22:30 +0100

    sdl mixer added

--- FlyEngine/Source/ModuleAudioManager.cpp
+++ FlyEngine/Source/ModuleAudioManager.cpp
@@ -1,4 +1,8 @@
 #include "ModuleAudioManager.h"
+#include "SDL.h"
+#include "SDL_mixer.h"
+
+#pragma comment(lib, "3rdParty/SDL_Mixer/Libs/SDL_mixer.lib")
 
 ModuleAudioManager::ModuleAudioManager(bool start_enabled)
 {
@@ -10,8 +14,8 @@ ModuleAudioManager::~ModuleAudioManager()
 
 bool ModuleAudioManager::Init()
 {
-	//if (SDL_Init(SDL_INIT_AUDIO) < 0)
-	//	return -1;
-
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+		return -1;
+	
 	return true;
 }
