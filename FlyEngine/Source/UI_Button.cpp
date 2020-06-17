e59e1d1488299d9dbf5572d08263cfc8d2445a79
rogerta <rogerteello97@gmail.com>
2020-06-17 17:58:28 +0200

    bugfix

--- FlyEngine/Source/UI_Button.cpp
+++ FlyEngine/Source/UI_Button.cpp
@@ -337,8 +337,8 @@ void UI_Button::Load(JSON_Object* jsonObject, string serializeStr)
 			ChangeRoomAction* newChangeRoomAction = uiObject->AddChangeRoomAction(); 
 			newChangeRoomAction->SetParentObject(uiObject); 
 			
-			UID dstRoomUID = json_object_dotget_number(jsonObject, string(actionStr + "Destination").c_str());
-			newChangeRoomAction->SetDestination(App->moduleWorldManager->GetRoom(dstRoomUID)); 
+			string dstRoom = json_object_dotget_string(jsonObject, string(actionStr + "Destination").c_str());
+			newChangeRoomAction->SetDestination(App->moduleWorldManager->GetRoom(dstRoom));
 
 			AddOnClickAction(newChangeRoomAction);
 		}
